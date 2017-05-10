478424fe48b6598cedbfe6a326a0522872331ffa
Isaac Evavold <isaacevavold@gmail.com>
2017-05-09 22:53:47 -0500

    basic init command

--- src/commands.ts
+++ src/commands.ts
@@ -1,3 +1,16 @@
+import { join } from 'path'
+import { mkdir } from 'fs'
+
 export function init () {
-  // TODO
+  mkdir(join(process.cwd(), '.tinygit'), (err) => {
+    if (err != null) {
+      if (err.code === 'EEXIST') {
+        console.error('.tinygit directory already exists')
+      } else if (err.code) {
+        console.error(err.code)
+      }
+
+      process.exit(err.errno || 1);
+    }
+  })
 }
