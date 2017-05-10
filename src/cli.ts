eb27729cdbbc1d9b96e464f5b527409f32ffb202
Isaac Evavold <isaacevavold@gmail.com>
2017-05-09 22:40:56 -0500

    add skeleton for `init` command

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,12 @@
 import * as program from 'commander'
+import * as commands from './commands'
 
 program
   .version('0.1.0')
+
+program
+  .command('init')
+  .description('Create an empty TinyGit repository')
+  .action(commands.init)
 
 program.parse(process.argv)
