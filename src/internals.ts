1ce45651201fd2708060bc3b39f5b3f16d6982d5
ievavold <ievavold@wisc.edu>
2017-05-10 11:12:29 -0500

    util function to check if repo folder exists

--- src/internals.ts
+++ src/internals.ts
@@ -1,3 +1,5 @@
+import { join } from 'path'
+import { existsSync } from 'fs'
 import { format } from 'util'
 import sha1 = require('sha1')
 
@@ -8,3 +10,17 @@ export function hashString (str: string): string {
 export function hashBlob (data: string): string {
   return hashString(format('blob %d\0%s', data.length, data))
 }
+export function exitIfRepoDoesNotExist (): boolean {
+  if (false === existsSync(repoDirpath())) {
+    console.error('Not a TinyGit repository')
+    process.exit(1)
+    return true
+  }
+
+  return false
+}
+
+export function repoDirpath (): string {
+  return join(process.cwd(), '.tinygit')
+}
+
