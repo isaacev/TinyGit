c7c44a829326ac3e358b887272aef07545947f44
ievavold <ievavold@wisc.edu>
2017-05-10 14:37:15 -0500

    briefly describe each TinyObject method

--- src/tiny-object.ts
+++ src/tiny-object.ts
@@ -1,8 +1,8 @@
 export interface TinyObject {
-  type     (): string
-  size     (): number
-  contents (): string
-  encode   (): string
-  hash     (): string
-  pretty   (): string
+  type     (): string // 'blob', 'tree', or 'commit'
+  size     (): number // size of contents in bytes
+  contents (): string // raw contents of object
+  encode   (): string // metadata prepended to raw contents
+  hash     (): string // SHA1 hash of encoded string
+  pretty   (): string // human-readable format of object data
 }
