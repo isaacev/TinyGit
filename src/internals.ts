c7574a4b86076dc783c8c00575c1a3adbee52d71
ievavold <ievavold@wisc.edu>
2017-05-10 11:15:15 -0500

    util function to write object to objects DB

--- src/internals.ts
+++ src/internals.ts
@@ -1,7 +1,10 @@
 import { join } from 'path'
-import { existsSync } from 'fs'
+import { existsSync, writeFile } from 'fs'
 import { format } from 'util'
 import sha1 = require('sha1')
+import * as mkdirp from 'mkdirp'
+
+type EmptyCallback = (err: NodeJS.ErrnoException) => void
 
 export function hashString (str: string): string {
   return String(sha1(str))
@@ -10,6 +13,28 @@ export function hashString (str: string): string {
 export function hashBlob (data: string): string {
   return hashString(format('blob %d\0%s', data.length, data))
 }
+
+export function writeObject (hash: string, data: string, done: EmptyCallback): void {
+  exitIfRepoDoesNotExist()
+
+  const prefix = hash.substring(0, 2)
+  const suffix = hash.substring(2)
+
+  mkdirp(objectsDirpath(prefix), (err) => {
+    if (err != null) {
+      return void done(err)
+    }
+
+    writeFile(objectsFilepath(prefix, suffix), data, (err) => {
+      if (err != null) {
+        return void done(err)
+      } else {
+        return void done(null)
+      }
+    })
+  })
+}
+
 export function exitIfRepoDoesNotExist (): boolean {
   if (false === existsSync(repoDirpath())) {
     console.error('Not a TinyGit repository')
@@ -24,3 +49,10 @@ export function repoDirpath (): string {
   return join(process.cwd(), '.tinygit')
 }
 
+export function objectsDirpath (prefix: string): string {
+  return join(repoDirpath(), 'objects', prefix)
+}
+
+export function objectsFilepath (prefix: string, suffix: string): string {
+  return join(objectsDirpath(prefix), suffix)
+}
