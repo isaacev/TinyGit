497a20c53a79bddc556ee90532966e55e2740d9e
Isaac Evavold <isaacevavold@gmail.com>
2017-05-21 22:44:08 -0500

    git add tests for util#hashString and util#repoDirpath

--- test/test-util.ts
+++ test/test-util.ts
@@ -1,2 +1,19 @@
 import 'mocha'
 import { expect } from 'chai'
+import * as path from 'path'
+
+import * as util from '../src/util'
+
+describe('util#hashString', () => {
+  it('should return a SHA1 hash', () => {
+    const val = util.hashString('hello')
+    expect(val).to.equal('aaf4c61ddcc5e8a2dabede0f3b482cd9aea9434d')
+  })
+})
+
+describe('util#repoDirpath', () => {
+  it('should return an absolute path to a local repository', () => {
+    const val = util.repoDirpath()
+    expect(val).to.equal(path.join(process.cwd(), '.tinygit'))
+  })
+})
