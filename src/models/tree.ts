61db57bdea2f99fe57e837ceeef509b906bcfa07
isaacev <isaacevavold@gmail.com>
2017-12-02 14:39:50 -0600

    fix encoding tag

--- src/models/tree.ts
+++ src/models/tree.ts
@@ -15,7 +15,7 @@ export class Tree implements Object {
   public id ()       : ID     { return this._id }
   public type ()     : 'tree' { return 'tree' }
   public size ()     : number { return this.contents().length }
-  public encode ()   : string { return fmt('blob %d\0%s', this.size(), this.contents()) }
+  public encode ()   : string { return fmt('tree %d\0%s', this.size(), this.contents()) }
   public contents () : string {
     return this._children.map(c => fmt('%s\0%s', c.name, c.id)).join('')
   }
