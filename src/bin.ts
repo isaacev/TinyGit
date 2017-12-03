67fe47707cf08c3ef88a66c5143e52b9cc3bdc99
isaacev <isaacevavold@gmail.com>
2017-12-03 03:31:45 -0600

    add generic error handler

--- src/bin.ts
+++ src/bin.ts
@@ -11,9 +11,19 @@ program
   .command('hash-object')
   .option('--write', 'writes blob to file', true)
   .arguments('<file>')
-  .action((file, options) => {
+  .action(errHandler((file, options) => {
     const id = plumbing.hashObject(file, options.write === true)
     console.log(id.toString())
-  })
+  }))
 
 program.parse(process.argv)
+
+function errHandler (action) {
+  return (...argv) => {
+    try {
+      action.apply(action, argv)
+    } catch (err) {
+      console.error(err.message)
+    }
+  }
+}
