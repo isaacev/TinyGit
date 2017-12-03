474f3caaa4e7b934705c0416bd9fbb1810870464
isaacev <isaacevavold@gmail.com>
2017-12-03 02:14:40 -0600

    implement hash-object command

--- src/bin.ts
+++ src/bin.ts
@@ -1,6 +1,19 @@
 #! /usr/bin/env node
 
-import { ID } from './models/object'
-import { Commit } from './models/commit'
-import { Tree } from './models/tree'
-import { Blob } from './models/blob'
+import * as program from 'commander'
+
+import * as plumbing from './plumbing'
+
+program
+  .version(require('../package.json').version)
+
+program
+  .command('hash-object')
+  .option('--write', 'writes blob to file', true)
+  .arguments('<file>')
+  .action((file, options) => {
+    const id = plumbing.hashObject(file, options.write === true)
+    console.log(id.toString())
+  })
+
+program.parse(process.argv)
---
+++ src/plumbing.ts
@@ -0,0 +1,31 @@
+import { format as fmt } from 'util'
+import * as path from 'path'
+import * as fs from 'fs'
+
+import * as io from './io'
+import { ID } from './models/object'
+import { Blob } from './models/blob'
+
+export const hashObject = (filepath: string, write: boolean = false): ID => {
+  if (path.isAbsolute(filepath) === false) {
+    filepath = path.resolve(filepath)
+  }
+
+  let data = ''
+  try {
+    data = fs.readFileSync(filepath, 'utf8')
+  } catch (err) {
+    throw new Error(fmt('failed to read %s', filepath))
+  }
+
+  const blob = new Blob(data)
+  if (write) {
+    try {
+      io.writeObject(blob)
+    } catch (err) {
+      throw new Error(fmt('failed to write blob %s', blob.id()))
+    }
+  }
+
+  return blob.id()
+}
