9f38179d734afc9b86472de63d79dc0254b6b105
isaacev <isaacevavold@gmail.com>
2017-12-03 03:33:03 -0600

    add method to read objects

--- src/io.ts
+++ src/io.ts
@@ -4,6 +4,9 @@ import * as fs from 'fs'
 import * as mkdirp from 'mkdirp'
 
 import { ID, Object } from './models/object'
+import { Commit } from './models/commit'
+import { Tree } from './models/tree'
+import { Blob } from './models/blob'
 
 export const getRepoRoot = (): string => {
   return path.join(process.cwd(), '.tinygit')
@@ -16,5 +19,25 @@ export const writeObject = (obj: Object): ID => {
     return obj.id()
   } catch (err) {
     throw new Error(fmt('failed to write object %s', obj.id.toString()))
+  }
+}
+
+export const readObject = (id: ID): Object => {
+  let raw = ''
+  try {
+    raw = fs.readFileSync(path.join(getRepoRoot(), 'objects', id.whole()), 'utf8')
+  } catch (err) {
+    throw new Error(fmt('failed to read object %s', id.toString()))
+  }
+
+  switch (true) {
+    case /^commit \d+\0/.test(raw):
+      return Commit.decode(raw)
+    case /^tree \d+\0/.test(raw):
+      return Tree.decode(raw)
+    case /^blob \d+\0/.test(raw):
+      return Blob.decode(raw)
+    default:
+      throw new Error(fmt('failed to decode %s', id.toString()))
   }
 }
